0c2a868147de1c9a76c4fec66f000399948c40ed
Ranjeetkuamr <ranjeetk5498@gmail.com>
2024-09-24 23:50:51 +0530

    structure_updated

---
+++ template.py
@@ -0,0 +1,46 @@
+import os
+from pathlib import Path
+
+import os
+from pathlib import Path
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/evaluate_model.py",
+    "src/pipeline/__init__.py",
+    "src/pipeline/training_pipeline.py",
+    "src/pipeline/prediction_pipeline.py",
+    "src/utils/__init__.py",
+    "src/utils/utils.py",
+    "src/logger/logging.py",
+    "src/exception/exception.py",
+    "tests/unit/__init__.py",
+    "tests/integration/__init__.py",
+    "init.setup.sh",
+    "requirements.txt",
+    "requirement_dev.txt",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "experiment/experiments.ipynb"
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    
+    # Only create the directory if `filedir` is not empty
+    if filedir:
+        os.makedirs(filedir, exist_ok=True)
+        
+    # Create an empty file if it doesn't exist or if it's empty
+    if not os.path.exists(filepath) or os.path.getsize(filepath) == 0:
+        with open(filepath, "w") as f:
+            pass  # create an empty file
