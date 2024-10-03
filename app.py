cde8eef9a225ea8f91fddf9c002ee4be98f2a280
Ranjeetkuamr <ranjeetk5498@gmail.com>
2024-10-04 03:51:31 +0530

    code update

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from src.pipeline.prediction_pipeline import CustomData,PredictPipeline
+
+application=Flask(__name__)
+
+app=application
+
+## Route for a home page
+
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            carat=float(request.form.get('carat')),
+            depth=float(request.form.get('depth')),
+            table=float(request.form.get('table')),
+            x=float(request.form.get('x')),
+            y=float(request.form.get('y')),
+            z=float(request.form.get('z')),
+            cut=request.form.get('cut'),
+            color=request.form.get('color'),
+            clarity=request.form.get('clarity')
+
+        )
+        pred_df=data.get_data_as_dataframe()
+        print(pred_df)
+      
+
+        predict_pipeline=PredictPipeline()
+        
+        results=predict_pipeline.predict(pred_df)
+        
+        return render_template('home.html',final_result=results[0])
+    
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=8000)
